3b6b2a5df67cd42ba418bf3b4b919ff1955083f2
yuncoffee <dong072815@gmail.com>
2023-12-19 21:32:15 +0900

    [Chore] Marker Test

---
+++ pins/Managers/MapkitManager.swift
@@ -0,0 +1,18 @@
+//
+//  MapManager.swift
+//  pins
+//
+//  Created by yuncoffee on 12/19/23.
+//
+
+import Foundation
+import MapKit
+
+@Observable
+final class MapkitManager {
+    
+}
+
+extension CLLocationCoordinate2D {
+    static let yangjuHome = CLLocationCoordinate2D(latitude: 37.818893, longitude: 127.100640)
+}
--- pins/Views/Main/MainView.swift
+++ pins/Views/Main/MainView.swift
@@ -6,18 +6,38 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MainView: View {
     @Environment(AuthManager.self)
     var authManager
     
+    @State
+    private var region = MKCoordinateRegion()
+    
     var body: some View {
-        VStack {
-            Text("Hello, World!")
-            Button {
-                authManager.signOut()
-            } label: {
-                Text("로그아웃")
+        ZStack {
+            Map(){
+                Marker(coordinate: .yangjuHome) {
+                    Text("Home")
+                }
+            }
+            .onAppear {
+                let manager = CLLocationManager()
+                manager.requestWhenInUseAuthorization()
+                manager.startUpdatingLocation()
+            }
+            //            {
+            //                Marker("San Francisco City Hall", coordinate: cityHallLocation)
+            //                                    .tint(.orange)
+            //            }
+            VStack {
+                Text("Hello, World!")
+                Button {
+                    authManager.signOut()
+                } label: {
+                    Text("로그아웃")
+                }
             }
         }
     }
